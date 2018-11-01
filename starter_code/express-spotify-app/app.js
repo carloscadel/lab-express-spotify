183b7a5d3850f0954166dcfe2a0eeb87b49577b7
carloscadel <c.cazallas.delfa@gmail.com>
2018-11-01 23:28:55 +0100

    Iteration 3 done

--- starter_code/express-spotify-app/app.js
+++ starter_code/express-spotify-app/app.js
@@ -1,11 +1,16 @@
 var SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express')
 const app = express()
+const path = require('path')
 const hbs = require('hbs')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Remember to paste your credentials here
 var clientId = 'f9e89ccc5a1a4d9880f4fd448618ff52',
-    clientSecret = 'ef4e36c310db4e859ab6a838330e48fd';
+    clientSecret = '399b54d361b944e386364f9e9ab4d828';
 
 var spotifyApi = new SpotifyWebApi({
   clientId : clientId,
@@ -19,3 +24,32 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+app.get('/', (req, res, next) => {
+    res.render('index')
+})
+
+app.get('/artists', (req, res, next) => {
+  // console.log('artists called')
+  // console.log('req ', req.query.artist)
+  spotifyApi.searchArtists(req.query.artist)
+  
+  .then(data => {
+      res.render('artists', {
+        artist: data.body.artists.items
+
+        
+        // console.log(artist)
+        // spotifyApi.searchArtists.items
+
+      })
+        console.log(data.body.artists.items[0].images)
+      })
+    .catch(err => {
+        
+      })
+})
+
+
+
+app.listen(3000)
